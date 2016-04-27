c709a14ff7dbb5629c9bea5607e9cdc8675c5ca2
Goryudyuma <umaretekyoumade@gmail.com>
2016-04-28 00:20:21 +0900

    少し修正

--- rescode/rescode.lua
+++ rescode/rescode.lua
@@ -1,3 +1,3 @@
 math.randomseed(os.time());
 rescode = {200, 201, 300, 301, 302, 303, 304, 400, 401, 403, 404, 405, 410, 500, 501, 503, 504};
-ngx.exit(rescode[math.random(1, 17)]);
+ngx.exit(rescode[math.random(#rescode)]);
